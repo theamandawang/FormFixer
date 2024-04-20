86cecc6fe41d5df542daedd55780cae40846d6a0
RoryHemmings <roryhemmings@ucla.edu>
2024-04-19 22:42:22 -0700

    Add skeleton code

---
+++ web/frontend/src/app.d.ts
@@ -0,0 +1,16 @@
+import { SupabaseClient, Session } from '@supabase/supabase-js'
+
+declare global {
+  namespace App {
+    interface Locals {
+      supabase: SupabaseClient
+      safeGetSession(): Promise<{ session: Session | null; user: User | null }>
+    }
+    interface PageData {
+      session: Session | null
+      user: User | null
+    }
+    // interface Error {}
+    // interface Platform {}
+  }
+}
